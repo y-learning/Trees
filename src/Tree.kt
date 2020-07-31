251f2fd300c4c9b363cf5af2886a188f572f513c
Yahya <yahyatinani@gmail.com>
2020-07-31 12:10:27 +0100

    Implement plus() function for Tree type

--- src/Tree.kt
+++ src/Tree.kt
@@ -2,6 +2,15 @@ sealed class Tree<out E : Comparable<@UnsafeVariance E>> {
 
     abstract fun isEmpty(): Boolean
 
+    operator fun plus(element: @UnsafeVariance E): Tree<E> = when (this) {
+        Empty -> T(Empty, element, Empty)
+        is T -> when {
+            element > this.root -> T(left, root, right + element)
+            element < this.root -> T(left + element, root, right)
+            else -> T(this.left, element, right)
+        }
+    }
+
     internal object Empty : Tree<Nothing>() {
         override fun isEmpty(): Boolean = true
 
