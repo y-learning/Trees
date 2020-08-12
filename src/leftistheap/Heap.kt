35b7af3a0501c8ef049a4de5cbbafbd9692c9cf4
Yahya <yahyatinani@gmail.com>
2020-08-12 12:40:00 +0100

    Implement plus() function for Heap type

--- src/leftistheap/Heap.kt
+++ src/leftistheap/Heap.kt
@@ -13,6 +13,8 @@ sealed class Heap<out E : Comparable<@UnsafeVariance E>> {
     abstract val size: Int
     abstract val isEmpty: Boolean
 
+    operator fun plus(e: @UnsafeVariance E): Heap<E> = merge(this, Heap(e))
+
     abstract class Empty<out E : Comparable<@UnsafeVariance E>> : Heap<E>() {
         override
         val head: Result<E> = Result.failure("head() called on empty heap")
@@ -33,7 +35,10 @@ sealed class Heap<out E : Comparable<@UnsafeVariance E>> {
     internal object E : Empty<Nothing>()
 
     internal class H<out E : Comparable<@UnsafeVariance E>>(
-            override val rank: Int, l: Heap<E>, h: E, r: Heap<E>) : Heap<E>() {
+            override val rank: Int,
+            private val l: Heap<E>,
+            private val h: E,
+            private val r: Heap<E>) : Heap<E>() {
 
         override val head: Result<E> = Result(h)
 
@@ -45,10 +50,45 @@ sealed class Heap<out E : Comparable<@UnsafeVariance E>> {
 
         override val isEmpty: Boolean = false
 
-        override fun toString(): String = "(T $left $head $right)"
+        override fun toString(): String = "(T $l $h $r)"
     }
 
     companion object {
         operator fun <E : Comparable<E>> invoke(): Heap<E> = E
+
+        operator fun <E : Comparable<E>> invoke(e: E): Heap<E> = H(1, E, e, E)
+
+        private fun <E : Comparable<E>> make(head: E,
+                                             first: Heap<E>,
+                                             second: Heap<E>): Heap<E> = when {
+            first.rank >= second.rank -> H(second.rank + 1, first, head, second)
+            else -> H(first.rank + 1, second, head, first)
+        }
+
+        fun <E : Comparable<E>> merge(heap1: Heap<E>, heap2: Heap<E>): Heap<E> =
+                heap1.head.flatMap { head1: E ->
+                    heap2.head.flatMap { head2: E ->
+                        when {
+                            head1 <= head2 -> {
+                                heap1.left.flatMap { left1: Heap<E> ->
+                                    heap1.right.map { right1: Heap<E> ->
+                                        make(head1, left1, merge(right1, heap2))
+                                    }
+                                }
+                            }
+
+                            else -> {
+                                heap2.left.flatMap { left2: Heap<E> ->
+                                    heap2.right.map { right2: Heap<E> ->
+                                        make(head2, left2, merge(right2, heap1))
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }.getOrElse(when (heap1) {
+                    E -> heap2
+                    else -> heap1
+                })
     }
 }
